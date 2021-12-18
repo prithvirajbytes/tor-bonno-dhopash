0162cffea3c831b2f94dc573025c14a388eed13f
prithvirajbytes <biswasprithviraj74@gmail.com>
2021-12-18 14:39:37 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# web_countdown_particles
+A responsive website for projects launch with countdown and cool JS particles
+
+Access it: [here](https://juanlamarao.github.io/web_countdown_particles/)
+
+TODO:
+  set a max @media for max-width..
---
+++ scripts/countdown.js
@@ -0,0 +1,27 @@
+//const referenceDate = new Date('Dec 26, 2021 10:00:00').getTime();
+const referenceDate = (new Date("Dec 26, 2021 10:00:00").getTime());
+
+const elDays = document.getElementById('days');
+const elHours = document.getElementById('hours');
+const elMinutes = document.getElementById('minutes');
+const elSeconds = document.getElementById('seconds');
+const elOutput = document.getElementById('output');
+
+var loop = setInterval(function () {
+	var currentDate = new Date().getTime();
+	var remainTime = referenceDate - currentDate;
+
+	elDays.innerHTML = Math.floor(remainTime / (1000 * 60 * 60 * 24));
+	elHours.innerHTML = Math.floor((remainTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	elMinutes.innerHTML = Math.floor((remainTime % (1000 * 60 * 60)) / (1000 * 60));
+	elSeconds.innerHTML = Math.floor((remainTime % (1000 * 60)) / 1000);
+
+	if (remainTime <= 0) {
+		clearInterval(loop);
+		elDays.innerHTML = 0;
+		elHours.innerHTML = 0;
+		elMinutes.innerHTML = 0;
+		elSeconds.innerHTML = 0;
+		elOutput.innerHTML = 'Tempo esgotado';
+	}
+}, 1000);
